8fa4a984bdf30590a53e1ede043fd4b6be91d7aa
Robby Costales <rcostales15@simons-rock.edu>
2017-03-15 23:54:47 -0400

    resolved all edit recommendations, minus 1

--- main.py
+++ main.py
@@ -3,16 +3,16 @@
 # Authors: Robert Costales
 # Assignment: K-means / K-medians methods
 # Date: 3/10/2017
+# Most Recent Code Update: 3/15/2017
 #
 # This program can use both the k-means and the k-medians method
 # to group points into clusters.
 
-#testing 1, 2, 3
 from sklearn import datasets
 import matplotlib.pyplot as plt
 import random
 import numpy as np
-import turtle
+import time
 from tkinter import *
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ RUN PREF _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- #
@@ -77,6 +77,55 @@ def manhattan(p1, p2):
     return d
 
 
+def calc_sst(list_pts):
+    """
+    :param list_pts:  list of all the points
+    :return: total sum of squares: sum of all the distances to the mean point
+    NOTE: works only for 2D currently
+    """
+    x = 0   # initialization of mean point x coordinate
+    y = 0   # initialization of mean point y coordinate
+    for j in list_pts:
+        x += j[0]
+        y += j[1]
+    x /= len(list_pts)
+    y /= len(list_pts)
+    mean_pt = [x, y]    # initialization of mean point
+
+    sst = 0
+    for j in list_pts:
+        sst += euclidean(j, mean_pt)
+    return sst
+
+
+def calc_ssb():
+    """
+    :return: sum of squares between:
+    NOTE: Have not made function because it is pretty useless
+    There is a '-' operator for a reason (SSB = SST - SSW).
+    I will just find SST and SSW first, and take their difference.
+    """
+    return
+
+
+def calc_ssw(center_list, clust_list, n):
+    """
+    :param center_list: list of center points for clusters
+    :param n: number of clusters
+    :param clust_list: list of all the points
+    :return: sum of squares within: (total, [nested list of individual values])
+    """
+    ssw_list = []
+    for i in range(n):
+        ssw_indy = 0
+        for j in clust_list[i]:
+            ssw_indy += euclidean(center_list[i], j)
+        ssw_list.append(ssw_indy)
+
+    ssw_total = sum(ssw_list)
+    return ssw_total, ssw_list
+
+
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ DATA _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- #
 
 iris = datasets.load_iris()
@@ -110,14 +159,22 @@ for i in range(num_clusters):
     a = random.randint(0, len(points) - 1)     # randomize centers
     centers.append(points[a][0:2])
 
+clusters = []
+for i in range(num_clusters):
+    clusters.append([])         # will contain sub-lists of all points in each cluster
+
 prev_centers = [0]
 if k_means:
-    print("K MEANS:\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n")
+    print("\nK MEANS:\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n")
     while centers[:] != prev_centers[:]:       # checks to see if any centers changed
+        for i in range(num_clusters):   # clears clusters list
+            clusters[i] = []
+
         for i in centers:
             print("[{0:.2f}, {1:.2f}]".format(i[0], i[1]), end=" ")
         print("")
         prev_centers = centers[:]
+
         # classify each point into a cluster (0 to num_clusters-1)
         for j in range(len(points)):  # j for each point
             shortest_distance = -1
@@ -126,6 +183,8 @@ if k_means:
                 if current_distance < shortest_distance or shortest_distance == -1:
                     shortest_distance = current_distance
                     points[j][2] = k
+            identity = points[j][2]
+            clusters[identity].append(points[j])
 
         # find new ideal center for each cluster
         for k in range(num_clusters):
@@ -140,14 +199,18 @@ if k_means:
             new_center = [x_count/count, y_count/count]
             centers[k] = new_center
 
-
+prev_centers = [0]
 if k_medians:
-    print("K MEDIANS:\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n")
+    print("\nK MEDIANS:\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n")
     while centers[:] != prev_centers[:]:       # checks to see if any centers changed
+        for i in range(num_clusters):   # clears clusters list
+            clusters[i] = []
+
         for i in centers:
             print("[{0:.2f}, {1:.2f}]".format(i[0], i[1]), end=" ")
         print("")
         prev_centers = centers[:]
+
         # classify each point into a cluster (0 to num_clusters-1)
         for j in range(len(points)):  # j for each point
             shortest_distance = -1
@@ -156,6 +219,8 @@ if k_medians:
                 if current_distance < shortest_distance or shortest_distance == -1:
                     shortest_distance = current_distance
                     points[j][2] = k
+            identity = points[j][2]
+            clusters[identity].append(points[j])
 
         # find new ideal center for each cluster
         for k in range(num_clusters):
@@ -169,6 +234,25 @@ if k_medians:
                     count += 1
             new_center = [np.median(x_temp), np.median(y_temp)]
             centers[k] = new_center
+
+# -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ CALCULATIONS _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- #
+
+SST = calc_sst(points)
+SSW = calc_ssw(centers, clusters, num_clusters)
+SSB = SST - SSW[0]
+
+print("\n\nCALCULATIONS:\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n")
+
+print("SST: {0:>8}".format(SST))
+print("SSW TOTAL: {0:>8}".format(SSW[0]))
+ssw_string = "[ "
+for i in SSW[1]:
+    ssw_string += str(i) + " "
+ssw_string += "]"
+print("SSW INDIVIDUAL: {0:>8}".format(ssw_string))
+print("SSB: {0:>8}".format(SSB))
+print("SSB / SST: {0:>8}\n".format(SSB/SST))
+print("Our model is {0:.3f}% accurate!".format(SSB/SST))
 
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ FINAL PLOTS _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- #
@@ -184,4 +268,5 @@ for i in range(len(points)):
 for i in range(len(centers)):
     plt.scatter(x=centers[i][0], y=centers[i][1], color=black)
 
+plt.title("Cluster Plot")
 plt.show()
